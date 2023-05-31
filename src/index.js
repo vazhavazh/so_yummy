982c5471cf27fe181f4bccfb48d9617a1103f236
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-05-31 10:23:34 +0300

    init2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
+import App from 'components/App';
 import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/react-wallet">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
