7d9a899ec959d323fc6bc9255699edbfde21ac50
EITalanova <elenabelichenko77@gmail.com>
2023-06-01 10:57:23 +0300

    Fixed

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { Hero } from '../components/Hero/Hero';
+// import { Hero } from '../components/Hero/Hero';
 // import { СhooseYourBreakfast } from '../components/СhooseYourBreakfast/СhooseYourBreakfast'; 
 // import { Search } from '../components/Search/Search'; 
-import { PreviewCategories } from '../components/PreviewCategories/PreviewCategories'; 
-import { MainWrapper } from '../components/MainWrapper/MainWrapper'; 
+// import { PreviewCategories } from '../components/PreviewCategories/PreviewCategories'; 
+// import { MainWrapper } from '../components/MainWrapper/MainWrapper'; 
 
 
 
