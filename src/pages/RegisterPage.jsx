565840c817c27528e8d599b3dc17868dda3f70bf
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-06-04 03:40:38 +0300

    add registration logic

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { registerUser } from 'redux/auth/authThunks';
 
 const RegisterPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default RegisterPage
+  const handleSubmit = e => {
+    e.preventDefault();
+    const userData = {
+      name,
+      email,
+      password,
+    };
+    console.log(userData)
+    dispatch(registerUser({...userData}));
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button type="submit">REGISTER</button>
+      </form>
+    </>
+  );
+};
+
+export default RegisterPage;
