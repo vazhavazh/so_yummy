7e5745946ff825ba2e1781ba40c630199a433b60
EITalanova <elenabelichenko77@gmail.com>
2023-06-04 16:24:07 +0300

    add useChangeScreen

--- src/components/PreviewCategories/PreviewCategories.jsx
+++ src/components/PreviewCategories/PreviewCategories.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { useChangeScreen } from 'hoc/useChangeScreen';
+
 import { Button } from '../Button/Button';
 import { RecipeCard } from '../RecipeCard/RecipeCard';
 import recipes from '../../api/fakeApi/fakeFavoriteDBcopy.json';
@@ -7,6 +9,21 @@ import recipes from '../../api/fakeApi/fakeFavoriteDBcopy.json';
 import style from '../PreviewCategories/PreviewCategories.module.scss';
 
 export const PreviewCategories = () => {
+  const screenWidthMobile = useChangeScreen(767.9);
+  const screenWidthTablet = useChangeScreen(1439.9);
+
+  const numberOfRecipes = recipes => {
+    if (screenWidthMobile) {
+      return recipes.slice(0, 1);
+    }
+    if (screenWidthTablet) {
+      return recipes.slice(0, 2);
+    }
+    if (window.innerWidth >= 1440) {
+      return recipes.slice(0, 4);
+    }
+  };
+
   return (
     <>
       <div className={style.previewCategoriesBox}>
@@ -14,7 +31,7 @@ export const PreviewCategories = () => {
           <li className={style.previewCategoriesListEll}>
             <p className={style.categoriesName}>Categorie name</p>
             <ul className={style.recipeList}>
-              {recipes.map(recipe => (
+              {numberOfRecipes(recipes).map(recipe => (
                 <RecipeCard key={recipe._id.$oid} recipe={recipe} />
               ))}
             </ul>
