6e9b260aeeb3841dd783387786366ebf83573afd
cldblz <cloudyshogun@gmail.com>
2023-06-05 01:10:56 +0600

    add burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useState } from 'react';
+import logo from '../../assets/svg/header/logo.svg';
+import burger from '../../assets/svg/header/burger.svg';
+import style from './Header.module.scss';
+import Menu from './Menu/Menu';
 
 export const Header = () => {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
   return (
-    <div>
-      Header
-    </div>
-  )
-}
-
-
+    <header>
+      <img src={logo} alt="logo" />
+      <img src="" alt="avatar" />
+      <p>Name</p>
+      <nav>
+        <button
+          className={style.burger}
+          type="button"
+          onClick={() => setIsMenuActive(true)}
+        >
+          <img src={burger} alt="" />
+        </button>
+        <Menu setIsMenuActive={setIsMenuActive} isMenuActive={isMenuActive} />
+      </nav>
+    </header>
+  );
+};
