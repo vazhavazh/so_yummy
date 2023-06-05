90f865ef09a5a5db4e3b10cb85c63b2477fca1fc
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-06-05 07:53:59 +0000

    Deploying to gh-pages from @ vazhavazh/so_yummy@cb20ba67ce55ed12586b0d78f0e945b52bf5af22 🚀

--- static/js/565.6df10ecc.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkso_yummy=self.webpackChunkso_yummy||[]).push([[565],{3565:function(u,s,e){e.r(s);e(2791);var n=e(3329);s.default=function(){return(0,n.jsx)("div",{})}}}]);
-//# sourceMappingURL=565.6df10ecc.chunk.js.map
