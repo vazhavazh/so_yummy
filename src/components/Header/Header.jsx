c3f78280ceb4249212aceb306c2589e1f71946b9
cldblz <cloudyshogun@gmail.com>
2023-06-05 21:59:17 +0600

    did header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import logo from '../../assets/svg/header/logo.svg';
 import burger from '../../assets/svg/header/burger.svg';
+import avatarPlaceholder from '../../assets/svg/header/avatar-placeholder.jpg';
 import style from './Header.module.scss';
 import Menu from './Menu/Menu';
 
@@ -8,20 +9,24 @@ export const Header = () => {
   const [isMenuActive, setIsMenuActive] = useState(false);
 
   return (
-    <header>
+    <header className={`${style.header} ${style.container}`}>
       <img src={logo} alt="logo" />
-      <img src="" alt="avatar" />
-      <p>Name</p>
-      <nav>
-        <button
-          className={style.burger}
-          type="button"
-          onClick={() => setIsMenuActive(true)}
-        >
-          <img src={burger} alt="" />
-        </button>
-        <Menu setIsMenuActive={setIsMenuActive} isMenuActive={isMenuActive} />
-      </nav>
+      <div className={style.userInfoWrapper}>
+        <img className={style.avatar} src={avatarPlaceholder} alt="avatar" />
+        <p className={style.username}>Name</p>
+        <nav className={style.nav}>
+          <button
+            className={style.burger}
+            type="button"
+            onClick={() => setIsMenuActive(true)}
+          >
+            <img src={burger} alt="" />
+          </button>
+          <Menu setIsMenuActive={setIsMenuActive} isMenuActive={isMenuActive} />
+        </nav>
+      </div>
     </header>
   );
 };
+
+console.log(style);
