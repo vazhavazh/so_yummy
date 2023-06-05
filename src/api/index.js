1774d8065d4655bd8d0d03c41b53b836feedfc68
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-06-05 03:50:02 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+// axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+axios.defaults.baseURL = 'https://so-yummy-mg49.onrender.com';
 
 
 
