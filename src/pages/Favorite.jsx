8087b80b11f36a1f6f4b2d2e924225048ed61f81
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-06-05 21:10:15 +0300

    upd

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,10 +1,12 @@
 import { FavoriteReceipts } from 'components/FavoriteReceipts/FavoriteReceipts';
 import React from 'react';
+import {MyComponent} from 'components/testComponent'
 
 const Favorite = () => {
   return (
     <>
       <FavoriteReceipts />
+      <MyComponent />
     </>
   );
 };
