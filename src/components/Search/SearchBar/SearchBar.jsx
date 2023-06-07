162891d8c21acd5fd672a22e67b0d40b66ae4e83
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-06-07 16:59:37 +0300

    fix imports bug

--- src/components/Search/SearchBar/SearchBar.jsx
+++ src/components/Search/SearchBar/SearchBar.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import SearchForm from '../SearchForm/SearchForm';
-import SearchTypeSelector from '../SearchTypeSelector/SearchTypeSelector';
+import React, { useState} from 'react';
 import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import scss from './SearchBar.module.scss';
 import { useDispatch } from 'react-redux';
 import { searchAllIngredients } from 'redux/search/searchThunks';
+import SearchForm from '../SearchForm/SearchForm';
+import SearchTypeSelector from '../SearchTypeSelector/SearchTypeSelector';
+import scss from './SearchBar.module.scss';
 
 const SearchBar = ({ value, onChange, onSearch }) => {
   const [prevSearchValue, setPrevSearchValue] = useState('');
