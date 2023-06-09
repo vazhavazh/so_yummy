6a6085ff037d22c3b76ba939761eb24a1b5d6856
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-06-09 12:19:55 +0300

    ...

--- src/components/AddRecipeForm/AddRecipeForm.jsx
+++ src/components/AddRecipeForm/AddRecipeForm.jsx
@@ -1,143 +1,216 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import { Box } from '@mui/material';
+// import { Form, Formik } from 'formik';
+// import * as Yup from 'yup';
+// import styles from './AddRecipeForm.module.scss';
+// import Button from './AddRecipeButton';
+// import categories from './data/categories.json';
+// import cookingTime from './data/cookingTime.json';
+// import { RecipeDescriptionFields } from './RecipeDescriptionFields';
+// import { RecipeIngredientsFields } from './RecipeIngredientsFields';
+// import { RecipePreparationFields } from './RecipePreparationFields/RecipePreparationFields';
+
+// const MAX_FILE_SIZE = 700 * 1024;
+
+// const initialValues = {
+//   title: '',
+//   about: '',
+//   category: 'breakfast',
+//   cookingTime: '40 min',
+//   recipe: '',
+//   file: '',
+//   ingredients: [{ name: '', dose: '' }],
+//   preparation: '',
+// };
+
+// const validFileExtensions = {
+//   image: ['jpg', 'png', 'jpeg', 'webp'],
+// };
+
+// function isValidFileType(fileName, fileType) {
+//   return (
+//     fileName &&
+//     validFileExtensions[fileType].indexOf(fileName.split('.').pop()) > -1
+//   );
+// }
+
+// const FORM_VALIDATION = Yup.object().shape({
+//   title: Yup.string().required('Title is required'),
+//   about: Yup.string().required('About is required'),
+//   category: Yup.string().required('Category is required'),
+//   cookingTime: Yup.string().required('Cooking time is required'),
+//   file: Yup.mixed()
+//     .test('is-valid-file', 'Invalid file format', function (value) {
+//       if (!value) {
+//         return true;
+//       }
+//       return isValidFileType(value && value.name.toLowerCase(), 'image');
+//     })
+//     .test(
+//       'is-valid-size',
+//       'File size exceeds the maximum limit',
+//       function (value) {
+//         if (!value) {
+//           return true;
+//         }
+//         return value.size <= MAX_FILE_SIZE;
+//       }
+//     )
+//     .required('Image is required'),
+//   ingredients: Yup.array()
+//     .of(
+//       Yup.object().shape({
+//         name: Yup.string().required('Ingredient is required'),
+//         dose: Yup.string().required('Dose is required'),
+//       })
+//     )
+//     .required('Ingredients are required'),
+//   preparation: Yup.string(),
+// });
+
+// export const AddRecipeForm = () => {
+//   const [isFormSubmitted, setFormSubmitted] = useState(false);
+//   const [counter, setCounter] = useState(1);
+
+// const handleSubmit = (values, { resetForm }) => {
+//   const file = values.file;
+//   const errorMessage = validateFile(file);
+//   if (!errorMessage) {
+//     console.log(values);
+//     setFormSubmitted(true);
+//     setCounter(1);
+//     resetForm();
+//   }
+// };
+
+//   const handleIncrement = () => {
+//     setCounter(prevCounter => prevCounter + 1);
+//   };
+
+//   const handleDecrement = () => {
+//     if (counter > 1) {
+//       setCounter(prevCounter => prevCounter - 1);
+//     }
+//   };
+
+//   const validateFile = file => {
+//     if (!file) {
+//       return 'Please select a file';
+//     }
+
+//     if (file.size > MAX_FILE_SIZE) {
+//       return 'File size exceeds the maximum limit';
+//     }
+
+//     if (!isValidFileType(file.name.toLowerCase(), 'image')) {
+//       return 'Invalid file format';
+//     }
+
+//     return undefined;
+//   };
+
+//   return (
+// <div className={styles.addRecipeForm}>
+//   <Formik
+//     initialValues={initialValues}
+//     validationSchema={FORM_VALIDATION}
+//     onSubmit={handleSubmit}
+//   >
+//     {({ values, errors }) => (
+//       <Form>
+//         <div className={styles.addRecipeForm}>
+//           <RecipeDescriptionFields
+//             isFormSubmitted={isFormSubmitted}
+//             categories={categories}
+//             cookingTime={cookingTime}
+//           />
+//           <RecipeIngredientsFields
+//             counter={counter}
+//             handleIncrement={handleIncrement}
+//             handleDecrement={handleDecrement}
+//           />
+//           <RecipePreparationFields />
+// <Box marginTop="18px" width="100%">
+//   <Button type="submit">Submit</Button>
+// </Box>
+//         </div>
+//         {/* <pre>{JSON.stringify(errors, null, 4)}</pre>
+//         <pre>{JSON.stringify(values, null, 4)}</pre> */}
+//       </Form>
+//     )}
+//   </Formik>
+// </div>
+//   );
+// };
+
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Box } from '@mui/material';
-import { Form, Formik } from 'formik';
 import * as Yup from 'yup';
 import styles from './AddRecipeForm.module.scss';
 import Button from './AddRecipeButton';
-import categories from './data/categories.json';
-import cookingTime from './data/cookingTime.json';
-import { RecipeDescriptionFields } from './RecipeDescriptionFields';
-import { RecipeIngredientsFields } from './RecipeIngredientsFields';
-import { RecipePreparationFields } from './RecipePreparationFields/RecipePreparationFields';
-
-const MAX_FILE_SIZE = 700 * 1024;
 
 const initialValues = {
   title: '',
   about: '',
-  category: 'breakfast',
-  cookingTime: '40 min',
-  recipe: '',
-  file: '',
-  ingredients: [{ name: '', dose: '' }],
-  preparation: '',
-  test: '',
 };
-
-const validFileExtensions = {
-  image: ['jpg', 'png', 'jpeg', 'webp'],
-};
-
-function isValidFileType(fileName, fileType) {
-  return (
-    fileName &&
-    validFileExtensions[fileType].indexOf(fileName.split('.').pop()) > -1
-  );
-}
 
 const FORM_VALIDATION = Yup.object().shape({
   title: Yup.string().required('Title is required'),
   about: Yup.string().required('About is required'),
-  category: Yup.string().required('Category is required'),
-  cookingTime: Yup.string().required('Cooking time is required'),
-  file: Yup.mixed()
-    .test('is-valid-file', 'Invalid file format', function (value) {
-      if (!value) {
-        return true;
-      }
-      return isValidFileType(value && value.name.toLowerCase(), 'image');
-    })
-    .test(
-      'is-valid-size',
-      'File size exceeds the maximum limit',
-      function (value) {
-        if (!value) {
-          return true;
-        }
-        return value.size <= MAX_FILE_SIZE;
-      }
-    )
-    .required('Image is required'),
-  ingredients: Yup.array()
-    .of(
-      Yup.object().shape({
-        name: Yup.string().required('Ingredient is required'),
-        dose: Yup.string().required('Dose is required'),
-      })
-    )
-    .required('Ingredients are required'),
-  preparation: Yup.string(),
-  test: Yup.string().required(),
 });
 
 export const AddRecipeForm = () => {
-  const [isFormSubmitted, setFormSubmitted] = useState(false);
-  const [counter, setCounter] = useState(1);
-
   const handleSubmit = (values, { resetForm }) => {
-    const file = values.file;
-    const errorMessage = validateFile(file);
-    if (!errorMessage) {
-      console.log(values);
-      setFormSubmitted(true);
-      setCounter(1);
-      resetForm();
-    }
-  };
-
-  const handleIncrement = () => {
-    setCounter(prevCounter => prevCounter + 1);
-  };
-
-  const handleDecrement = () => {
-    if (counter > 1) {
-      setCounter(prevCounter => prevCounter - 1);
-    }
-  };
-
-  const validateFile = file => {
-    if (!file) {
-      return 'Please select a file';
-    }
-
-    if (file.size > MAX_FILE_SIZE) {
-      return 'File size exceeds the maximum limit';
-    }
-
-    if (!isValidFileType(file.name.toLowerCase(), 'image')) {
-      return 'Invalid file format';
-    }
-
-    return undefined;
+    console.log(values);
+    resetForm();
   };
 
   return (
-    <div className={styles.addRecipeForm}>
+    <div className={styles.addRecipeFormWrapper}>
       <Formik
         initialValues={initialValues}
         validationSchema={FORM_VALIDATION}
         onSubmit={handleSubmit}
       >
-        {({ values, errors }) => (
-          <Form>
-            <div className={styles.addRecipeForm}>
-              <RecipeDescriptionFields
-                isFormSubmitted={isFormSubmitted}
-                categories={categories}
-                cookingTime={cookingTime}
+        {({ values, errors, touched }) => (
+          <Form className={styles.addRecipeForm}>
+            <div className={`${styles.inputWrapper}`}>
+              <Field
+                className={`${styles.recipeDescriptionInput} ${
+                  errors.title && touched.title ? styles.error : ''
+                }`}
+                type="text"
+                name="title"
+                placeholder="Enter item title"
               />
-              <RecipeIngredientsFields
-                counter={counter}
-                handleIncrement={handleIncrement}
-                handleDecrement={handleDecrement}
+              <ErrorMessage
+                className={styles.errorMessage}
+                name="title"
+                component="div"
               />
-              <RecipePreparationFields />
-              <Box marginTop="18px" width="100%">
-                <Button type="submit">Submit</Button>
-              </Box>
             </div>
-            <pre>{JSON.stringify(errors, null, 4)}</pre>
-            <pre>{JSON.stringify(values, null, 4)}</pre>
+            <div className={`${styles.inputWrapper}`}>
+              <Field
+                className={`${styles.recipeDescriptionInput} ${
+                  errors.about && touched.about ? styles.error : ''
+                }`}
+                type="text"
+                name="about"
+                placeholder="Enter about recipe"
+              />
+              <ErrorMessage
+                name="about"
+                component="div"
+                className={styles.errorMessage}
+              />
+            </div>
+            <Box marginTop="18px" width="100%">
+              <Button type="submit">Add</Button>
+            </Box>
+
+            {/* <pre>{JSON.stringify(errors, null, 4)}</pre>
+            <pre>{JSON.stringify(values, null, 4)}</pre> */}
           </Form>
         )}
       </Formik>
--- src/pages/AddRecipe.jsx
+++ src/pages/AddRecipe.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { AddRecipeForm } from 'components/AddRecipeForm/AddRecipeForm';
+import { Box } from '@mui/material';
 
 const AddRecipe = () => {
-  return <AddRecipeForm />;
+  return (
+    <Box width="100%" margin="0 auto" padding="20px">
+      <AddRecipeForm />
+    </Box>
+  );
 };
 
 export default AddRecipe;
