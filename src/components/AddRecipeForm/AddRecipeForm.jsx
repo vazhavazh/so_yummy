34e27094046cc8d029e2ee70cdea17d44245231b
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-06-09 13:51:04 +0300

    ...

--- src/components/AddRecipeForm/AddRecipeForm.jsx
+++ src/components/AddRecipeForm/AddRecipeForm.jsx
@@ -147,18 +147,65 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Box } from '@mui/material';
 import * as Yup from 'yup';
+import ReactSelect from 'react-select';
 import styles from './AddRecipeForm.module.scss';
 import Button from './AddRecipeButton';
+import categories from './data/categories.json';
+import cookingTime from './data/cookingTime.json';
 
 const initialValues = {
   title: '',
   about: '',
+  category: '',
 };
 
 const FORM_VALIDATION = Yup.object().shape({
   title: Yup.string().required('Title is required'),
   about: Yup.string().required('About is required'),
+  category: Yup.string().required('Category is required'),
 });
+
+const customStyles = {
+  menu: provided => ({
+    ...provided,
+    background: 'transparent',
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    backgroundColor: state.isFocused ? '#D9D9D9' : 'white',
+    color: state.isFocused ? 'white' : 'black',
+  }),
+  input: provided => ({
+    ...provided,
+    color: '#8BAA36',
+  }),
+  dropdownIndicator: (provided, state) => ({
+    ...provided,
+    border: 'none',
+    boxShadow: 'none',
+    color: '#8BAA36', // Зміна коліру стрілки при наведенні
+  }),
+  control: (provided, state) => ({
+    ...provided,
+    height: 34,
+    width: 143,
+    background: '#D9D9D9',
+    borderRadius: 6,
+    borderColor: state.isFocused ? '#8BAA36' : '#CED4DA', // Зміна коліру рамки при наведенні
+    boxShadow: state.isFocused ? '0 0 0 2px #8BAA36' : 'none', // Зміна тіні рамки при наведенні
+    '&:hover': {
+      borderColor: state.isFocused ? '#8BAA36' : '#CED4DA', // Зміна коліру рамки при наведенні
+    },
+    '@include tablet': {
+      width: 175,
+      height: 41,
+    },
+    '@include desktop': {
+      width: 192,
+      height: 49,
+    },
+  }),
+};
 
 export const AddRecipeForm = () => {
   const handleSubmit = (values, { resetForm }) => {
@@ -173,9 +220,9 @@ export const AddRecipeForm = () => {
         validationSchema={FORM_VALIDATION}
         onSubmit={handleSubmit}
       >
-        {({ values, errors, touched }) => (
+        {({ values, errors, touched, handleChange, setFieldValue }) => (
           <Form className={styles.addRecipeForm}>
-            <div className={`${styles.inputWrapper}`}>
+            <div className={styles.inputWrapper}>
               <Field
                 className={`${styles.recipeDescriptionInput} ${
                   errors.title && touched.title ? styles.error : ''
@@ -190,7 +237,7 @@ export const AddRecipeForm = () => {
                 component="div"
               />
             </div>
-            <div className={`${styles.inputWrapper}`}>
+            <div className={styles.inputWrapper}>
               <Field
                 className={`${styles.recipeDescriptionInput} ${
                   errors.about && touched.about ? styles.error : ''
@@ -205,12 +252,30 @@ export const AddRecipeForm = () => {
                 className={styles.errorMessage}
               />
             </div>
+            <div className={styles.inputWrapperCategory}>
+              <label className={styles.categoryLabel} htmlFor="category">
+                Search by:
+              </label>
+              <ReactSelect
+                // className={styles.search_select}
+                name="category"
+                options={categories}
+                // styles={customStyles}
+                getOptionLabel={option => option.label} //можна буде змінити label, якщо на беку він буде підписаний по-іншому
+                getOptionValue={option => option.value} //можна буде змінити label, якщо на беку він буде підписаний по-іншому
+                isSearchable={false}
+                value={categories.find(
+                  option => option.value === values.category
+                )}
+                onChange={option => setFieldValue('category', option.value)}
+              />
+            </div>
             <Box marginTop="18px" width="100%">
               <Button type="submit">Add</Button>
             </Box>
 
-            {/* <pre>{JSON.stringify(errors, null, 4)}</pre>
-            <pre>{JSON.stringify(values, null, 4)}</pre> */}
+            <pre>{JSON.stringify(errors, null, 4)}</pre>
+            <pre>{JSON.stringify(values, null, 4)}</pre>
           </Form>
         )}
       </Formik>
