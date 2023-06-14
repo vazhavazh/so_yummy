be41e85060ab66b8d288032c9ded82d9f282fdad
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-06-14 22:29:45 +0300

    Update MyRecipe2.jsx

---
+++ src/pages/MyRecipe2.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import style from 'components/RecipeMain/RecipeMain.module.scss';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+import { RecipeIngredientsList } from 'components/RecipeMain/RecipeIngredientsList';
+import { RecipePreparation } from 'components/RecipeMain/RecipePreparation';
+import { fetchMyOwnRecipe } from 'redux/pipka/pipkaThunk';
+import selectMyRecipes from 'redux/pipka/pipkaSelector';
+import { RecipePageHeroMy } from 'components/MyownRecipeComponent/RecipePageHeroMy';
+
+const MyRecipe2 = () => {
+  const { recipeId } = useParams();
+  const dispatch = useDispatch();
+  const recipe = useSelector(selectMyRecipes);
+  const ingredients = recipe.ingredients;
+  const instructions = recipe.instructions;
+  const img = recipe.preview
+
+
+
+  useEffect(() => {
+    dispatch(fetchMyOwnRecipe(recipeId));
+  }, [dispatch, recipeId]);
+
+  return (
+    <>
+      <div className={style.body}>
+        <RecipePageHeroMy recipe={recipe} />
+        <RecipeIngredientsList ingredients={ingredients} />
+        <RecipePreparation instructions={instructions} img={img} />
+      </div>
+    </>
+  );
+};
+
+export default MyRecipe2;
