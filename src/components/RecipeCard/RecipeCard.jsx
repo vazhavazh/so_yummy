e6c8c6c20819196a0fb6fbc953d25d54d9a5c16a
Andrii <lutsiukandrey1@gmail.com>
2023-06-14 14:35:08 +0300

    fixed route to recipe

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import style from '../RecipeCard/RecipeCard.module.scss';
 import { ReactComponent as IconTime } from '../../assets/svg/mainPage/icon-time.svg';
+import { NavLink } from 'react-router-dom';
 
 export const RecipeCard = ({ recipe }) => {
   const { title, preview, _id, time, description } = recipe;
   return (
     <li className={style.recipeEll}>
-      <a href={`so_yummy/recipe/${_id}`}>
+      {/* <a href={`so_yummy/recipe/${_id}`}> */}
+      <NavLink to={`/recipe/${_id}`}>
         <img className={style.recipeImg} src={preview} alt="recipe prewiew" />
         <p className={style.recipeTitle}>{title}</p>
         <div className={style.recipeOverlay}>
@@ -17,7 +19,8 @@ export const RecipeCard = ({ recipe }) => {
           </p>
           <p className={style.recipeText}>{description}</p>
         </div>
-      </a>
+      </NavLink>
+      {/* </a> */}
     </li>
   );
 };
