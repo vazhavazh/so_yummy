93c955a5cd42df4c3dde556d25f88700a6214721
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-08-01 22:00:02 +0300

    Update authThunks.js

--- src/redux/auth/authThunks.js
+++ src/redux/auth/authThunks.js
@@ -54,10 +54,8 @@ export const getCurrentUser = createAsyncThunk(
       return data;
     } catch (error) {
       if (error.response.status === 401) {
-        // clearAuthHeader();
-        Notify (401)
-        // window.localStorage.clear();
-
+        clearAuthHeader();
+        window.localStorage.clear();
       } else {
         return rejectWithValue(error.message);
       }
